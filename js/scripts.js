1e3240614fff6e592b4cd7dbe076403949e9a5b5
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-03-22 17:25:20 -0700

    add javascript, get registration to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,47 @@
 // Business Logic
 
-function Account(withdraw, deposit) {
+function Account(initial ,withdraw, deposit) {
+  this.initial = initial;
   this.withdraw = withdraw;
   this.deposit = deposit;
 }
 
+Account.prototype.newBalance = function() {
+  var total = initial
+  if (this.withdraw) {
+    return (total - this.withdraw);
+  } else if (this.deposit) {
+    return (this.deposit + total);
+  } else {
+    return total;
+  }
+};
+
+
 //User Input
 
-$(document).ready(function(event) {
-  event.preventDefault();
+$(document).ready(function() {
 
-  $('form#newAccount').submit(function() {
+
+  $('form#newAccount').submit(function(event) {
+    event.preventDefault();
     var inputtedIntial = parseInt($('#initial').val());
-  });
+    $('#total').show();
+    $('#total h2').text(inputtedIntial);
+    });
 
-  $('form#accountChange').submit(function() {
+  $('form#accountChange').submit(function(event) {
+    event.preventDefault();
+    var inputtedIntial = parseInt($('#initial').val());
     var inputtedDeposit = parseInt($('#deposit').val());
     var inputtedWithdrawal = parseInt($('#withdraw').val());
+
+    var operation = new Account(inputtedInitial, inputtedWithdrawal, inputtedDeposit);
+
+    $('#total').show();
+    $('#total h2').text(total);
+
+
+    var total = operation.newBalance();
   });
 });
